223d61c9e4ca2ac6f68fcd83abac227299d4216d
Reinhard Pointner <rednoah@filebot.net>
2024-04-13 19:20:15 +0700

    Remove ANSI color codes for terminal emulator compatibility

--- generic/opt/share/activate.sh
+++ generic/opt/share/activate.sh
@@ -18,17 +18,14 @@ docker run --rm -it -v data:/data -e PUID=$(id -u) -e PGID=$(id -g) rednoah/file
 
 if [ "$(id -u)" -eq 0 ]; then
 echo '
-\033[91m
 	!!! YOU ARE RUNNING AS ROOT AND NOT AS NORMAL USER !!!
-\033[0m
 '
 fi
 
 
 echo "
-\033[93m
+# env
 USER=$(id -un)($(id -u))
 HOME=$HOME
-\033[0m
 "
 
