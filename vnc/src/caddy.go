028707654f71be827da9b40fbbc88ee7d7f6e153
Reinhard Pointner <rednoah@filebot.net>
2024-06-24 14:15:06 +0700

    Remove filebot-vnc

--- vnc/src/caddy.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import caddycmd "github.com/caddyserver/caddy/v2/cmd"
-import _ "github.com/caddyserver/caddy/v2/modules/standard"
-import _ "github.com/mholt/caddy-webdav"
-
-func main() {
-	caddycmd.Main()
-}
