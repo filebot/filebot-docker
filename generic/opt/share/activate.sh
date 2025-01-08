fdc08a15f8a7d982207f3842dcdff41051321c28
Reinhard Pointner <rednoah@filebot.net>
2025-01-09 01:12:52 +0700

    YOU DID NOT BIND MOUNT /data TO A PERSISTENT HOST FOLDER

--- generic/opt/share/activate.sh
+++ generic/opt/share/activate.sh
@@ -16,15 +16,6 @@ docker run --rm -it -v data:/data -e PUID=$(id -u) -e PGID=$(id -g) rednoah/file
 "
 
 
-if [ "$(id -u)" -eq 0 ]; then
-echo '
-\033[38;5;196m
-	!!! YOU ARE RUNNING AS ROOT AND NOT AS NORMAL USER !!!
-\033[0m
-'
-fi
-
-
 echo "
 \033[38;5;214m
 # env
@@ -32,3 +23,24 @@ USER=$(id -un)($(id -u))
 HOME=$HOME
 \033[0m
 "
+
+
+if [ "$(id -u)" -eq 0 ]; then
+echo "
+\033[38;5;196m
+	!!! YOU ARE RUNNING AS ROOT AND NOT AS NORMAL USER !!!
+\033[0m
+"
+fi
+
+
+if [ ! -d "$HOME" ] || [ "$(stat --file-system --format '%T' "$HOME")" = 'overlayfs' ]; then
+echo "
+\033[38;5;196m
+	!!! YOU DID NOT BIND MOUNT $HOME TO A PERSISTENT HOST FOLDER !!!
+
+	All data stored to the application data folder \`$HOME\` will be lost on container shutdown, like tears in rain.
+	Please add \`-v data:/data\` to your \`docker\` command lest your application data, such as license key, be lost in time.
+\033[0m
+"
+fi
